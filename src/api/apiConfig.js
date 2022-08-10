f129e7b26df2ce6b92e470f64f06e62d04d9bd48
David Aremu <segun8427@gmail.com>
2022-08-10 02:22:58 +0100

    reversed token change

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: process.env.API_KEY,
+    apiKey: '4fb7582b04ffdc34132d87358ccdf684',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
